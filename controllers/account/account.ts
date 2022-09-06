339172ffe50ad6c856f13284c80e516411e52c4c
AzerothA <telifesite@gmail.com>
2022-09-06 16:08:05 +0430

    New Update

--- controllers/account/account.ts
+++ controllers/account/account.ts
@@ -1,6 +1,15 @@
-import { Controller, Schema } from 'lib';
+import { Controller, SchemaValidator } from 'lib';
+import { Model } from "denodb";
+import { VerifyCode } from 'models';
 
-const schema = new Schema({ phoneNumber: "number" });
+const sendCodeSchema = new SchemaValidator({
+    email: { type: "string", required: true, maxLength: 30 }
+});
+
+const verifyCodeSchema = new SchemaValidator({
+    email: { type: "string", required: true, maxLength: 30 },
+    code: { type: "number", required: true, maxLength: 6 }
+});
 
 class Account extends Controller {
     get() {
@@ -8,10 +17,11 @@ class Account extends Controller {
         return Promise.resolve(new Response("GET FUCK"));
     }
 
-    // @actions({ "login": this.login, "register": this.register })
     post() {
         if (this.router.getAction === "verify") {
             return this.verify();
+        } else if (this.router.getAction === "sendCode") {
+            return this.sendCode();
         }
         return Promise.resolve(new Response("Action is not Correct"))
     }
@@ -20,11 +30,58 @@ class Account extends Controller {
         // Get Phone number from Body
         const body = await this.request.json();
 
-        if (schema.check(body) === false) {
-            return new Response("Request body is not correct!")
+        await verifyCodeSchema.validate(body);
+
+        const lastSendedCode = await VerifyCode.select("email", "code")
+            .where({ email: body.email })
+            .orderBy("created_at", "desc")
+            .limit(1)
+            .first() as Model;
+
+        if (!lastSendedCode)
+            return new Response("You are Not in", { status: 400 })
+
+        if (lastSendedCode.code === body.code) {
+            // Update Status Of Code
+            VerifyCode.where("code", lastSendedCode.code as number).update({ status: "login" });
+
+            return new Response("You are in !");
         }
 
-        return new Response("GET FUCK");
+        return new Response("You are Not in", { status: 400 })
+    }
+
+    async sendCode(): Promise<Response> {
+        const body = await this.request.json();
+
+        await sendCodeSchema.validate(body);
+
+        const randomCode = await this.generateRandomCode();
+        const lastSendedCode = await VerifyCode.select("code", "created_at").where({ email: body.email })
+            .orderBy("created_at", "desc")
+            .limit(1)
+            .first() as Model;
+
+        if (lastSendedCode) {
+            const currentDate = Date.now();
+            const createdAtAsMilliSecond = Date.parse(lastSendedCode.createdAt! as string);
+
+            if (currentDate - createdAtAsMilliSecond < 5000) {
+                return Response.json({ code: lastSendedCode.code })
+            }
+        }
+
+        VerifyCode.create({ status: "waiting", code: randomCode, email: body.email });
+
+        return Response.json({ code: randomCode });
+    }
+
+    private generateRandomCode(length = 6): Promise<number> {
+        const min = Math.pow(10, (length - 1));
+        const max = Math.pow(10, (length));
+        const val = Math.floor(Math.random() * (max - min) + min);
+
+        return Promise.resolve(val);
     }
 }
 
