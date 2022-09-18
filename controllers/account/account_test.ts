181c1b2e0b93bd81b979fc6f5e3251b3336a2e63
AzerothA <telifesite@gmail.com>
2022-09-18 14:45:09 +0430

    New Test Added

--- controllers/account/account_test.ts
+++ controllers/account/account_test.ts
@@ -1,5 +1,7 @@
-import { assertEquals } from "std@test";
+import { assertEquals, assertRejects } from "std@test";
+import Account from "./account.ts";
 import { Token } from "./token.ts";
+import { Router } from 'lib';
 
 Deno.test("Token", async () => {
     const token = new Token(1);
@@ -7,4 +9,14 @@ Deno.test("Token", async () => {
     await token.generate();
 
     assertEquals(token.getToken.length, 32);
+});
+
+Deno.test("Random Code", async () => {
+    const exampleRequest = new Request("http://localhost:8080/");
+    const exampleRouter = new Router("/account/");
+
+    const account = new Account(exampleRequest, exampleRouter);
+    assertRejects(() => account.verify());
+
+    assertEquals(await (await account.post!()).text(), "Action is not Correct");
 });
