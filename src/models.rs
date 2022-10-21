59babb09decd91326db4c3cdc533a65f895b2247
AzerothA <telifesite@gmail.com>
2022-10-21 17:47:24 +0330

    New update

--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,21 @@
+use crate::schema::app_verify_code;
 use chrono::NaiveDateTime;
-use diesel::Queryable;
+use diesel::{Insertable, Queryable};
 
 #[derive(Queryable, Debug)]
 pub struct VerifyCode {
-    id: i32,
-    status: Option<String>,
-    code: Option<i32>,
-    email: Option<String>,
+    pub id: i32,
+    pub code: i32,
+    pub email: String,
+    pub status: String,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = app_verify_code)]
+pub struct NewVerifyCode<'a> {
+    pub status: &'a String,
+    pub code: &'a i32,
+    pub email: &'a String,
 }
--- src/routers.rs
+++ src/routers.rs
@@ -1,46 +1,73 @@
 pub mod account {
-    use crate::models::VerifyCode;
-    use crate::schema::app_verify_codes::dsl::*;
+    use crate::models::{NewVerifyCode, VerifyCode};
+    use crate::schema::app_verify_code::dsl::*;
     use crate::DbPool;
     use actix_web::{post, web, HttpResponse};
     use diesel::prelude::*;
     use rand::Rng;
     use serde::Deserialize;
 
-    fn generate_random_code(min: u32, max: u32) -> u32 {
-        let num: u32 = rand::thread_rng().gen_range(min..max);
+    const MIN_RANDOM_CODE: i32 = 100000;
+    const MAX_RANDOM_CODE: i32 = 999999;
+
+    fn generate_random_code(min: i32, max: i32) -> i32 {
+        let num: i32 = rand::thread_rng().gen_range(min..max);
 
         num
     }
 
-    #[derive(Deserialize)]
-    pub struct AccountInfo {
-        email: Option<String>,
+    #[derive(Deserialize, Clone)]
+    pub struct SendCodeInfo {
+        email: String,
     }
 
     #[post("/account/sendCode")]
-    pub async fn send_code(pool: web::Data<DbPool>, info: web::Json<AccountInfo>) -> HttpResponse {
-        let random_code = generate_random_code(100000, 999999);
+    pub async fn send_code(pool: web::Data<DbPool>, info: web::Json<SendCodeInfo>) -> HttpResponse {
+        let random_code = generate_random_code(MIN_RANDOM_CODE, MAX_RANDOM_CODE);
+        let mut conn = pool.get().unwrap();
 
-        let last_sended_code = web::block(move || {
-            let mut conn = pool.get().unwrap();
-            app_verify_codes
-                .filter(email.eq(info.email.clone()))
+        let response = web::block(move || {
+            let last_sended_code = app_verify_code
+                .filter(email.eq(&info.email))
                 .order_by(created_at)
                 .limit(1)
                 .load::<VerifyCode>(&mut conn)
-                .unwrap()
+                .unwrap();
+
+            if last_sended_code.len() > 0 {
+                let current_date = chrono::offset::Local::now().timestamp();
+                let last_code_date = last_sended_code[0].created_at.timestamp();
+
+                if (current_date - last_code_date) < 5000 {
+                    // TODO: Send same code here, do not create a new code
+                    return "Code sended".to_string();
+                }
+            }
+
+            let new_code = NewVerifyCode {
+                code: &random_code,
+                email: &info.email,
+                status: &"notUsed".to_string(),
+            };
+
+            diesel::insert_into(app_verify_code)
+                .values(&new_code)
+                .execute(&mut conn)
+                .unwrap();
+
+            // TODO: Send code Here
+
+            return "Code sended".to_string();
         })
         .await
         .unwrap();
 
-        if last_sended_code.len() > 0 {
-            let current_date = chrono::offset::Local::now();
+        HttpResponse::Ok().body(response)
+    }
 
-            println!("{:?}", current_date);
-            println!("{}", last_sended_code[0].created_at);
-        }
+    pub async fn verify(pool: web::Data<DbPool>, info: web::Json<AccountInfo>) -> HttpResponse {
+        let mut conn = pool.get().unwrap();
 
-        HttpResponse::Ok().body(format!("{}", random_code))
+        web::block(move || {})
     }
 }
