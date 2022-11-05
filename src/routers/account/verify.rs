c3feb6d710cdb44553ee19e495e8d38d3df90d1f
AzerothA <telifesite@gmail.com>
2022-11-05 14:18:40 +0330

    Status codes changed, bug fix

--- src/routers/account/verify.rs
+++ src/routers/account/verify.rs
@@ -56,6 +56,24 @@ pub struct VerifyCodeInfo {
     code: i32,
 }
 
+enum UserStatus {
+    /// This means user created at verifying email
+    Created,
+
+    /// This means user exists and just wants to get token (login)
+    Exists,
+}
+
+impl UserStatus {
+    /// Returns the status code of UserStatus
+    pub fn as_status_code(&self) -> StatusCode {
+        match *self {
+            UserStatus::Created => StatusCode::CREATED,
+            UserStatus::Exists => StatusCode::OK,
+        }
+    }
+}
+
 /// Verify verification code that sended to email
 /// from /account/sendCode router
 #[post("/account/verify")]
@@ -80,11 +98,18 @@ pub async fn verify(
             .unwrap();
 
         if last_sended_code.is_empty() {
-            return ("Code is not valid".to_string(), StatusCode::NOT_FOUND);
+            return (
+                "No coded sended to this email".to_string(),
+                StatusCode::GONE,
+            );
+        }
+
+        if last_sended_code[0].code != info.code {
+            return ("Code is not correct".to_string(), StatusCode::OK);
         }
 
         if last_sended_code[0].status == *"used".to_string() {
-            return ("Code is not valid".to_string(), StatusCode::NOT_FOUND);
+            return ("Code is already used".to_string(), StatusCode::OK);
         }
 
         let diff = time_deference(last_sended_code[0].created_at.time());
@@ -95,10 +120,6 @@ pub async fn verify(
             // address is known. This condition is expected to be considered permanent.
 
             return ("Code expired".to_string(), StatusCode::GONE);
-        }
-
-        if last_sended_code[0].code != info.code {
-            return ("Code is not correct".to_string(), StatusCode::NOT_FOUND);
         }
 
         // Everything is ok now change code status to used
@@ -115,7 +136,7 @@ pub async fn verify(
 
         // If we dont have user with request (email) then create it
         // else return it
-        let user: User = if user_from_db.is_empty() {
+        let (user, user_status): (User, UserStatus) = if user_from_db.is_empty() {
             let user = NewUser {
                 email: &info.email,
                 username: &"".to_string(),
@@ -131,11 +152,11 @@ pub async fn verify(
                 .execute(&mut conn)
                 .unwrap();
 
-            new_user
+            (new_user, UserStatus::Created)
         } else {
             let u = user_from_db.get(0).unwrap();
 
-            u.clone()
+            (u.clone(), UserStatus::Exists)
         };
 
         // Some salts
@@ -178,7 +199,7 @@ pub async fn verify(
             .execute(&mut conn)
             .unwrap();
 
-        (result, StatusCode::OK)
+        (result, user_status.as_status_code())
     })
     .await
     .unwrap();
