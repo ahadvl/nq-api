d34c3bc125320470ee4ed4e4d81a359b2f756aa4
AzerothA <telifesite@gmail.com>
2022-12-11 15:41:30 +0330

    Tokens table updated

--- migrations/2022-10-25-070029_create_app_token/up.sql
+++ migrations/2022-10-25-070029_create_app_token/up.sql
@@ -4,6 +4,8 @@ CREATE TABLE app_tokens (
     id serial NOT NULL,
     user_id serial NOT NULL,
     token_hash VARCHAR(64) NOT NULL,
+    terminated BOOLEAN NOT NULL DEFAULT false,
+    terminated_by_id serial,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT app_tokens_id PRIMARY KEY (id)
