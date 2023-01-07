720da3be62ff633d01a20f4cdd0bd9191d65a654
AzerothA <telifesite@gmail.com>
2023-01-07 16:02:30 +0330

    test

--- src/routers/quran/test.rs
+++ src/routers/quran/test.rs
@@ -5,26 +5,26 @@ mod tests {
     use actix_web::{http::StatusCode, test, web, App};
     use diesel::r2d2::Pool;
 
-    #[test]
-    async fn get_surah() {
-        let pool = Pool::builder()
-            .build(establish_database_connection())
-            .expect("Cant connect to db");
+    // #[test]
+    // async fn get_surah() {
+    //     let pool = Pool::builder()
+    //         .build(establish_database_connection())
+    //         .expect("Cant connect to db");
 
-        let app = test::init_service(
-            App::new()
-                .app_data(web::Data::new(pool.clone()))
-                .service(web::resource("/quran").route(web::get().to(quran))),
-        )
-        .await;
+    //     let app = test::init_service(
+    //         App::new()
+    //             .app_data(web::Data::new(pool.clone()))
+    //             .service(web::resource("/quran").route(web::get().to(quran))),
+    //     )
+    //     .await;
 
-        let req = test::TestRequest::get()
-            .uri("/quran?from=1&to=1")
-            .to_request();
+    //     let req = test::TestRequest::get()
+    //         .uri("/quran?from=1&to=1")
+    //         .to_request();
 
-        let resp = test::call_service(&app, req).await;
-        assert_eq!(resp.status(), StatusCode::OK);
-    }
+    //     let resp = test::call_service(&app, req).await;
+    //     assert_eq!(resp.status(), StatusCode::OK);
+    // }
 
     #[test]
     async fn send_bad_url() {
