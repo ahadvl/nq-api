cb5ab2a48c670b75166b165f5d8bc9d3da85a7dc
AzerothA <telifesite@gmail.com>
2023-03-24 01:27:59 +0330

    table updated

--- migrations/2023-03-12-100253_create_translation/up.sql
+++ migrations/2023-03-12-100253_create_translation/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE translations (
     id serial NOT NULL,
     translator_id serial NOT NULL,
-    language serial NOT NULL,
-    release_year DATE NOT NULL,
+    language VARCHAR(5) NOT NULL,
+    release_year DATE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT translation_id PRIMARY KEY (id)
