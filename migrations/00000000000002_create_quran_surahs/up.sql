020c0a7dd8868a2c23251e18b738070cc4c17c96
AzerothA <telifesite@gmail.com>
2023-04-10 12:08:44 +0330

    not null removed

--- migrations/00000000000002_create_quran_surahs/up.sql
+++ migrations/00000000000002_create_quran_surahs/up.sql
@@ -3,7 +3,7 @@
 CREATE TABLE quran_surahs (
     id serial NOT NULL,
     name VARCHAR(50) NOT NULL, 
-    period VARCHAR(50) NOT NULL, 
+    period VARCHAR(50), 
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT quran_surahs_id PRIMARY KEY (id)
