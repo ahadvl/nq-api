1ace4f3257c449e69911b449423d8a2a31ec0a30
AzerothA <telifesite@gmail.com>
2023-04-26 21:42:09 +0330

    redo

--- src/routers/account/mod.rs
+++ src/routers/account/mod.rs
@@ -13,6 +13,7 @@ pub const MAX_RANDOM_CODE: i32 = 999999;
 /// Get deference between Current time and past_time
 pub fn time_deference(past_time: NaiveDateTime) -> Duration {
     let current_date = Utc::now();
+    let past_date_time = DateTime::<Utc>::from_utc(past_time, Utc);
 
-    current_date.signed_duration_since(past_time)
+    current_date.signed_duration_since(past_date_time)
 }
