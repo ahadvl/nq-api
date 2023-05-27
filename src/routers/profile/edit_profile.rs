1dcace188fd40ed42825b96c89fa36f444eb8bc7
AzerothA <telifesite@gmail.com>
2023-05-27 21:51:20 +0330

    Now we can edit the primary name

--- src/routers/profile/edit_profile.rs
+++ src/routers/profile/edit_profile.rs
@@ -1,21 +1,33 @@
 use actix_web::web::{self, ReqData};
+use chrono::NaiveDate;
 use diesel::prelude::*;
+use serde::Deserialize;
 
 use crate::{
     error::RouterError,
-    models::{Account, User, UserProfile},
+    models::{Account, User, UserName},
     DbPool,
 };
 
-// TODO: Create a router for handling names
+#[derive(Deserialize)]
+pub struct EditableProfile {
+    pub username: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub birthday: NaiveDate,
+    pub profile_image: String,
+    pub language: String,
+}
+
 /// Edit the profile
 /// wants a new profile and token
 pub async fn edit_profile(
     data: ReqData<u32>,
     pool: web::Data<DbPool>,
-    new_profile: web::Json<UserProfile>,
+    new_profile: web::Json<EditableProfile>,
 ) -> Result<String, RouterError> {
     use crate::schema::app_accounts::dsl::{app_accounts, id as acc_id, username};
+    use crate::schema::app_user_names::dsl::{first_name, last_name, primary_name};
     use crate::schema::app_users::dsl::*;
 
     let user_id = data.into_inner();
@@ -54,8 +66,6 @@ pub async fn edit_profile(
         // And update the other data
         let Ok(_) = diesel::update(current_user_profile)
             .set((
-                // first_name.eq(new_profile.first_name),
-                // last_name.eq(new_profile.last_name),
                 birthday.eq(new_profile.birthday),
                 profile_image.eq(new_profile.profile_image),
             ))
@@ -63,6 +73,26 @@ pub async fn edit_profile(
             else {
                 return Err(RouterError::InternalError);
             };
+
+        // Also edit the primary name
+
+        // First We get the user_names of the account
+        // We assume that user has at least primary name
+        let Ok(names) = UserName::belonging_to(account)
+            .filter(primary_name.eq(true))
+            .load::<UserName>(&mut conn) else {
+                return Err(RouterError::InternalError);
+            };
+
+        let name: &UserName = names.get(0).unwrap();
+
+        // Now we update it
+        let Ok(_) = diesel::update(name).set((
+            first_name.eq(new_profile.first_name),
+            last_name.eq(new_profile.last_name),
+        )).execute(&mut conn) else {
+            return Err(RouterError::InternalError);
+        };
 
         Ok("Edited".to_string())
     })
