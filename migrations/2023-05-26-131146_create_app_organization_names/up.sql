31feb3600e8c341f888c767d2a14e51bbd5c80c4
AzerothA <telifesite@gmail.com>
2023-05-29 18:15:26 +0330

    migrations updated

--- migrations/2023-05-26-131146_create_app_organization_names/up.sql
+++ migrations/2023-05-26-131146_create_app_organization_names/up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE app_organization_names (
     id serial NOT NULL,
     account_id serial NOT NULL,
-    primary_name boolean NOT NULL,
     name VARCHAR(300) NOT NULL,
     language VARCHAR(4) NOT NULL,
     CONSTRAINT fk_org_names_account_id FOREIGN KEY(account_id) REFERENCES app_accounts(id),
