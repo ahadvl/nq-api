8026260fe7475f96a28b024d13694138f2b174e2
AzerothA <telifesite@gmail.com>
2023-05-29 18:16:22 +0330

    here

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -2,3 +2,29 @@ pub mod account;
 pub mod organization;
 pub mod profile;
 pub mod quran;
+
+use std::collections::BTreeMap;
+use std::hash::Hash;
+
+/// finds the relatives in the vector
+/// Vec<(Obj1, Obj2)>
+/// This will collect the Obj2 that related to the Obj1 and returns
+/// a BTreeMap (We want the elements be in order)
+pub fn multip<T, U, F, NT>(vector: Vec<(T, U)>, insert_data_type: F) -> BTreeMap<NT, Vec<U>>
+where
+    T: Sized + Clone,
+    U: Sized,
+    NT: Sized + Eq + Hash + Ord,
+    F: Fn(T) -> NT,
+{
+    let mut map: BTreeMap<NT, Vec<U>> = BTreeMap::new();
+    for item in vector {
+        if let Some(w) = map.get_mut(&insert_data_type(item.0.clone())) {
+            w.push(item.1)
+        } else {
+            map.insert(insert_data_type(item.0), vec![item.1]);
+        }
+    }
+
+    map
+}
