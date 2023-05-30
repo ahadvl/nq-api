3b66e5705ccd178350b5efa23aeb4ac2e5232c9e
AzerothA <telifesite@gmail.com>
2023-05-30 14:18:15 +0330

    migrations updated

--- migrations/2022-12-27-171201_create_app_organizations/up.sql
+++ migrations/2022-12-27-171201_create_app_organizations/up.sql
@@ -6,7 +6,6 @@ CREATE TABLE app_organizations(
     profile_image TEXT,
     established_date DATE NOT NULL,
     national_id VARCHAR(11) NOT NULL,
-    language VARCHAR(4) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT app_organizations_id PRIMARY KEY (id),
