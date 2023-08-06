08c7aba2ea2eb49f9413373ecaeac6f47cba18c5
AzerothA <telifesite@gmail.com>
2023-08-06 11:53:49 +0330

    new tables updated

--- migrations/2023-08-06-081247_create_app_permission_conditions/up.sql
+++ migrations/2023-08-06-081247_create_app_permission_conditions/up.sql
@@ -1,1 +1,9 @@
--- Your SQL goes here
+CREATE TABLE app_permission_conditions (
+    id serial NOT NULL,
+    permission_id serial NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    CONSTRAINT app_permission_conditions_id PRIMARY KEY (id),
+    UNIQUE(permission_id),
+    CONSTRAINT fk_cond_perm_id FOREIGN KEY(permission_id) REFERENCES app_permissions(id)
+);
