946f81a6f569c9203ade174c0bc21aa2ac37a57d
AzerothA <telifesite@gmail.com>
2023-08-17 13:26:08 +0330

    TokenGenerator -> HashBuilder

--- authN/src/lib.rs
+++ authN/src/lib.rs
@@ -1,1 +1,13 @@
 pub mod token;
+
+#[cfg(test)]
+mod tests {
+    use crate::token::HashBuilder;
+
+    #[test]
+    fn test_token_generator() {
+        let token = HashBuilder::new(&String::from("Hello World").into_bytes())
+            .generate()
+            .get_result();
+    }
+}
