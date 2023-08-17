e2fbee04703aa1f64ce8478502bff4a5a66d5bb7
AzerothA <telifesite@gmail.com>
2023-08-17 13:56:59 +0330

    test addded

--- authN/src/lib.rs
+++ authN/src/lib.rs
@@ -5,9 +5,15 @@ mod tests {
     use crate::token::HashBuilder;
 
     #[test]
-    fn test_token_generator() {
-        let token = HashBuilder::new(&String::from("Hello World").into_bytes())
+    fn test_hash_builder() {
+        let token = HashBuilder::new()
+            .set_source(&String::from("Hello World").into_bytes())
             .generate()
             .get_result();
+
+        assert_eq!(
+            token.unwrap(),
+            "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e".to_string()
+        );
     }
 }
