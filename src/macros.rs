8c8c3d46e68558f3b5b3e63bd7ecc72ad9383fa2
AzerothA <telifesite@gmail.com>
2023-08-20 16:01:59 +0330

    Some comments added

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,5 @@
-
 #[macro_export]
+/// This macro is used to impl the SelectModel for models
 macro_rules! select_model {
     ($struct:ty, $table_name:ident) => {
 
@@ -13,10 +13,12 @@ macro_rules! select_model {
                 block(move || {
                     // Get the Required Resource
                     let selected_model: Vec<$struct> = $table_name::$table_name
+                        // Filter it by id (select by id)
                         .filter($table_name::id.eq(id))
                         .load(&mut conn)
                         .unwrap();
 
+                    // Get the first item of vec
                     let selected_model = selected_model.get(0).unwrap();
 
                     selected_model.clone()
