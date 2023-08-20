444bd4c3219086f9bef9837d051b4cfe699a6f27
AzerothA <telifesite@gmail.com>
2023-08-20 15:48:28 +0330

    new modules, and macro that impls the SelectModel trati

--- src/macros.rs
+++ src/macros.rs
@@ -1,16 +1,19 @@
+
+#[macro_export]
 macro_rules! select_model {
     ($struct:ty, $table_name:ident) => {
+
         #[async_trait]
         impl SelectModel for $struct {
             async fn from_id(conn: DbPool, id: i32) -> Self {
-                use crate::schema::$table_name::dsl as ($table_name)_table;
+                use crate::schema::$table_name::dsl as $table_name;
 
                 let mut conn = conn.get().unwrap();
 
-                web::block(move || {
+                block(move || {
                     // Get the Required Resource
-                    let selected_model: Vec<$struct> = ($table_name)_table::app_organizations
-                        .filter(($table_name)_table::id.eq(id))
+                    let selected_model: Vec<$struct> = $table_name::$table_name
+                        .filter($table_name::id.eq(id))
                         .load(&mut conn)
                         .unwrap();
 
