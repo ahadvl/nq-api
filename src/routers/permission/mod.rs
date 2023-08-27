d443a932bc716af591cdd0a85837a64a7292e0d1
AzerothA <telifesite@gmail.com>
2023-08-27 12:03:13 +0330

    file renamed

--- src/routers/permission/mod.rs
+++ src/routers/permission/mod.rs
@@ -1,1 +1,13 @@
-pub mod view_permission;
+use crate::models::PermissionCondition;
+
+pub mod permissions_list;
+
+
+#[derive(Debug, Clone)]
+pub struct PermissionWithConditions {
+    subject: String,
+    object: String,
+    action: String,
+    conditions: Vec<PermissionCondition>,
+}
+
--- src/routers/permission/permissions_list.rs
+++ src/routers/permission/permissions_list.rs
@@ -1,18 +1,10 @@
 use crate::{
     error::RouterError,
     models::{Permission, PermissionCondition},
-    DbPool,
+    DbPool, routers::permission::PermissionWithConditions,
 };
 use actix_web::web;
 use diesel::prelude::*;
-
-#[derive(Debug, Clone)]
-struct PermissionWithConditions {
-    subject: String,
-    object: String,
-    action: String,
-    conditions: Vec<PermissionCondition>,
-}
 
 /// Returns the list of Permissions
 ///
@@ -23,13 +15,15 @@ pub async fn get_list_of_permissions(
     use crate::schema::app_permission_conditions::dsl::app_permission_conditions;
     use crate::schema::app_permissions::dsl::app_permissions;
 
-    let permissions = web::block(move || {
+    let permissions: Result<PermissionWithConditions, RouterError> = web::block(move || {
         let mut conn = pool.get().unwrap();
 
-        let permissions_with_conditions: Vec<()> = app_permissions
+        let permissions_with_conditions: Vec<(Permission, PermissionCondition)> = app_permissions
             .inner_join(app_permission_conditions)
             .select((Permission::as_select(), PermissionCondition::as_select()))
-            .load(&mut conn);
+            .load(&mut conn)?;
+
+        todo!()
     })
     .await
     .unwrap();
