34067301f26bda8effe6332730c9f2f2b4c41d16
AzerothA <telifesite@gmail.com>
2023-09-02 22:53:43 +0330

    on delete cascade added

--- migrations/2023-08-06-081247_create_app_permission_conditions/up.sql
+++ migrations/2023-08-06-081247_create_app_permission_conditions/up.sql
@@ -7,5 +7,6 @@ CREATE TABLE app_permission_conditions (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT app_permission_conditions_id PRIMARY KEY (id),
-    CONSTRAINT fk_cond_perm_id FOREIGN KEY(permission_id) REFERENCES app_permissions(id)
+    CONSTRAINT fk_cond_perm_id FOREIGN KEY(permission_id) REFERENCES app_permissions(id) 
+        on delete cascade
 );
