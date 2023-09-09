223daefd6910b2171f9f3d5a7d6490eef5bc74ce
AzerothA <telifesite@gmail.com>
2023-09-09 23:36:13 +0330

    Updated

--- src/routers/quran/mushaf/mod.rs
+++ src/routers/quran/mushaf/mod.rs
@@ -1,2 +1,14 @@
 pub mod mushaf_list;
+pub mod mushaf_add;
 pub mod mushaf_view;
+pub mod mushaf_edit;
+pub mod mushaf_delete;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct SimpleMushaf {
+    name: String,
+    source: String,
+}
+
