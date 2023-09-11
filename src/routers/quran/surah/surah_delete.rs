953eca975c6f14019252d1df2e6e251cd6e209ef
AzerothA <telifesite@gmail.com>
2023-09-11 23:48:03 +0330

    surah delete router added

--- src/routers/quran/surah/surah_delete.rs
+++ src/routers/quran/surah/surah_delete.rs
@@ -1,5 +1,8 @@
+use std::str::FromStr;
+
 use crate::{error::RouterError, DbPool};
 use actix_web::web;
+use diesel::prelude::*;
 use uuid::Uuid;
 
 /// Delete's the specific surah
@@ -7,13 +10,19 @@ pub async fn surah_delete<'a>(
     path: web::Path<String>,
     pool: web::Data<DbPool>,
 ) -> Result<&'a str, RouterError> {
+    use crate::schema::quran_surahs::dsl::{quran_surahs, uuid as surah_uuid};
+
     let path = path.into_inner();
 
     web::block(move || {
         let mut conn = pool.get().unwrap();
-        let uuid = Uuid::from_str();
+        let uuid = Uuid::from_str(&path)?;
+
+        diesel::delete(quran_surahs.filter(surah_uuid.eq(uuid))).execute(&mut conn)?;
 
         // Remove the Surah by uuid
         Ok("Deleted")
-    }).await.unwrap()
+    })
+    .await
+    .unwrap()
 }
